4e5cd2df1a03c2e8ccf812e8f27cfaff7b6d7198
Jackson Riley <jackson.riley@btinternet.com>
2021-02-28 20:31:45 +0000

    Add profile binary for use with flamegraph, and current flamegraph

---
+++ src/bin/prof.rs
@@ -0,0 +1,23 @@
+//! Binary to use with flamegraph to figure out the slow bits of move generation
+
+use cress::engine::ChessState;
+
+fn main() {
+    let state = ChessState::new_game();
+    assert_eq!(generate_moves(4, &state), 197_281);
+}
+
+fn generate_moves(depth: u8, state: &ChessState) -> u64 {
+    if depth == 0 {
+        1
+    } else {
+        let mut sum = 0;
+        for chess_move in state.generate_all_legal_moves(&state.player_turn) {
+            let mut new_state = state.clone();
+            // Make move
+            new_state.make_move_unchecked(&chess_move);
+            sum += generate_moves(depth - 1, &new_state);
+        }
+        sum
+    }
+}
